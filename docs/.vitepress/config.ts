9cc77e77750b6c6736665e96cbeb9db8974093d8
fangfeiqiang <2816574137@qq.com>
2026-01-16 23:51:09 +0800

    修改ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.vuejs.org/config/app-configs
 export default defineConfig({
  // 如果你的仓库名是 my-blog，则 base 必须是：
-  base: '/live-share/',
+  base: '/live-share/'
 
 })
 
