5c98afdafbf51ae30424f5bb4081dec70c4c653d
fangfeiqiang <2816574137@qq.com>
2026-01-16 20:13:45 +0800

    feat: initial vitepress live share

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.vuejs.org/config/app-configs
+export default defineConfig({})
+
+export default {
+  // 如果你的仓库名是 my-blog，则 base 必须是：
+  base: '/live-share/',
+
+  // 其他配置...
+}
